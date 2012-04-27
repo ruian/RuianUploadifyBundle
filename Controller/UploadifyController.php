f27d2f049e1ced5f9795e9e9aca105e7e258e6b2
Julien 'ruian' Galenski <galenski.julien@gmail.com>
2012-04-27 16:32:44 +0200

    add new controller for default upload

---
+++ Controller/UploadifyController.php
@@ -0,0 +1,25 @@
+<?php
+namespace Ruian\UploadifyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class UploadifyController extends Controller
+{
+    public function uploadAction(Request $request)
+    {
+        // Get options from the DIC
+        $options = $this->container->get('uploadify.options');
+
+        $filename = $options['fileObjName'];
+        $folder   = $options['folderUpload'];
+        $web_dir  = $this->container->getParameter('kernel_dir') . '/../web/';
+
+        $file = $request->files->get($filename);
+        $file->move($web_dir. '/' .$folder);
+
+        $response = new Response($file->getFilename());
+
+        return $response;
+    }
+}
